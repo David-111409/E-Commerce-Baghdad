6f67a531b1c42c2dbea8fd9c899a7d208280a037
David-111409 <muhammaddawoud610@gmail.com>
2026-03-09 07:26:20 +0200

    some work on cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import "bootstrap-icons/font/bootstrap-icons.css";
 import Header from "./components/header/Header";
+import { Route, Routes } from "react-router-dom";
+import Cart from "./pages/cart/Cart";
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/cart/Cart.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import "./cart.css"
+const Cart = () => {
+  return (
+    <div className="empty-cart">
+      <h1>سله التسوق الخاصه بك فارغه</h1>
+      <p>لا یوجد سلع</p>
+      <Link className="empty-cart-link" to="/products">
+        صفحه السلع
+      </Link>
+    </div>
+  );
+};
+
+export default Cart;
