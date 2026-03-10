93d4402032a82e599ff4cd694994e5a9b40838af
David-111409 <muhammaddawoud610@gmail.com>
2026-03-11 00:57:05 +0200

    category finish

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from "./components/header/Header";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/cart/Cart";
 import Banner from "./components/banner/Banner";
+import Category from "./components/category/Category";
 const App = () => {
   return (
     <>
       <Header />
       <Banner />
+      <Category />
       <Routes>
         <Route path="/cart" element={<Cart />} />
       </Routes>
