6c5c8d29d013929e2915c0209968f778fdbd9814
David-111409 <muhammaddawoud610@gmail.com>
2026-03-11 03:00:18 +0200

    special offers start

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Route, Routes } from "react-router-dom";
 import Cart from "./pages/cart/Cart";
 import Banner from "./components/banner/Banner";
 import Category from "./components/category/Category";
+import SpecialOffers from "./components/special-offers/SpecialOffers";
 const App = () => {
   return (
     <>
       <Header />
       <Banner />
       <Category />
+      <SpecialOffers />
       <Routes>
         <Route path="/cart" element={<Cart />} />
       </Routes>
---
+++ src/components/special-offers/SpecialOffers.jsx
@@ -0,0 +1,16 @@
+import "./special-offers.css";
+
+const SpecialOffers = () => {
+  return (
+    <div className="special-offers">
+      <h1 className="special-offers-title">
+        عروض کبیره للیوم{" "}
+        <span className="special-offers-icon-wrapper">
+          <i className="bi bi-stopwatch"></i> ل 24 ساعه بس
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default SpecialOffers;
